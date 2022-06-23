7498a54caccd2fe173a827ee2b1481de9433a08a
Santiago G <sgonzalez@modyo.com>
2022-06-22 19:41:00 -0500

    feat: added eslint

--- demos/src/02-math.js
+++ demos/src/02-math.js
@@ -1,21 +1,21 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divice(a, b) {
-    if (b === 0) {
-        return null;
-    };
+  if (b === 0) {
+    return null;
+  }
 
-    return a / b;
+  return a / b;
 }
 
 module.exports = {
-    sum,
-    multiply,
-    divice,
+  sum,
+  multiply,
+  divice,
 };
--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,25 +1,25 @@
 const { sum, multiply, divice } = require('./02-math');
 
 test('adds 1 + 3 should be 4', () => {
-    const rta = sum(1, 3);
-    expect(rta).toBe(4);
+  const rta = sum(1, 3);
+  expect(rta).toBe(4);
 });
 
 test('should be 4', () => {
-    const rta = multiply(1, 4);
-    expect(rta).toBe(4);
+  const rta = multiply(1, 4);
+  expect(rta).toBe(4);
 });
 
 test('should divide', () => {
-    const rta = divice(6, 3);
-    expect(rta).toBe(2);
-    const rta2 = divice(5, 2);
-    expect(rta2).toBe(2.5);
+  const rta = divice(6, 3);
+  expect(rta).toBe(2);
+  const rta2 = divice(5, 2);
+  expect(rta2).toBe(2.5);
 });
 
 test('should divide for zero', () => {
-    const rta = divice(6, 0);
-    expect(rta).toBeNull();
-    const rta2 = divice(5, 0);
-    expect(rta2).toBeNull();
+  const rta = divice(6, 0);
+  expect(rta).toBeNull();
+  const rta2 = divice(5, 0);
+  expect(rta2).toBeNull();
 });
