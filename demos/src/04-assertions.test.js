0fda3f4244547deda4103b435515bcbcb6dd7ba8
Santiago G <sgonzalez@modyo.com>
2022-06-23 20:20:08 -0500

    feat(04-assertions): added some matchers

---
+++ demos/src/04-assertions.test.js
@@ -0,0 +1,31 @@
+// matchers
+
+test('test obj', () => {
+  const data = { name: 'Santiago' };
+  expect(data).toEqual({ name: 'Santiago' });
+});
+
+test('null', () => {
+  const data = null;
+  expect(data).toBeNull();
+  expect(data).toBeDefined();
+  expect(data).not.toBeUndefined();
+});
+
+test('boolenas', () => {
+  expect(false).toEqual(false);
+  expect(true).toEqual(true);
+
+  expect(0).toBeFalsy();
+  expect('').toBeFalsy();
+  expect(false).toBeFalsy();
+});
+
+test('string', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+test('list / arrays', () => {
+  const numbers = [1, 2, 3, 4];
+  expect(numbers).toContain(3);
+});
