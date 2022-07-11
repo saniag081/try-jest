dc7794c7bd580bec8654b3f8e19cfe826ec68cac
Santiago G <sgonzalez@modyo.com>
2022-07-10 20:32:08 -0500

    feat(e2e): added e2e test

---
+++ api/e2e/hello.e2e.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+
+const createApp = require('../src/app');
+
+describe('Test for hello endpoint', () => {
+  let app = null;
+  let server = null;
+
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('Test for [GET] /', () => {
+    test('Should return "Hello word!"', () => request(app)
+      .get('/')
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toEqual('Hello World!');
+      }));
+  });
+});
