c05ce44b810b03c0d3b06cf29bebed84f07018ff
Santiago G <sgonzalez@modyo.com>
2022-07-21 20:57:16 -0500

    feat: added data base for testing

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,36 +1,54 @@
-const mockSpyGetAll = jest.fn();
 const request = require('supertest');
-const createApp = require('../src/app');
-const { generateManyBooks } = require('../src/fakes/book.feke');
+const { MongoClient } = require('mongodb');
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockSpyGetAll,
-  create: () => {},
-})));
+const createApp = require('../src/app');
+const { config } = require('../src/config');
+
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
 describe('Test for Books', () => {
   let app = null;
   let server = null;
+  let dataBase = null;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    await client.connect();
+    dataBase = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    dataBase.dropDatabase();
   });
 
   describe('Test for [GET] /api/v1/books', () => {
-    test('Should return a list books', () => {
-      const fakeBooks = generateManyBooks(4);
-      mockSpyGetAll.mockResolvedValue(fakeBooks);
+    test('Should return a list books', async () => {
+      const seedData = await dataBase.collection('books').insertMany([
+        {
+          name: 'books1',
+          year: 1998,
+          author: 'prueba',
+        },
+        {
+          name: 'books1',
+          year: 1998,
+          author: 'prueba',
+        },
+      ]);
 
       return request(app)
         .get('/api/v1/books')
         .expect(200)
         .then(({ body }) => {
-          expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
